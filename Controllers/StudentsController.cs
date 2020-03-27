4888102414e22b364363dbec75686640273586bc
s19342 <61838002+s19342@users.noreply.github.com>
2020-03-27 12:08:56 +0100

    Initial commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APBD3.DAL;
+using APBD3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Student not found");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database
+            //... generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+
+            return Ok(student);
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id)
+        {
+            return Ok("Update complete");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Delete completed");
+        }
+    }
+}
---
+++ DAL/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APBD3.Models;
+
+namespace APBD3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
